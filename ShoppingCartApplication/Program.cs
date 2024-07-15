7ac35d453beb9ca6ee6ed2e136d2dd8244cdc897
yazanMhussein <yazan.m.hussein@gmail.com>
2024-07-15 10:38:28 +0300

    Add project files.

---
+++ ShoppingCartApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartCartApplication.ShoppingCartApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
